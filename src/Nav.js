a83a5019d15863824963b89e83c278973229dfdb
dev-thakku <jemshithtk@gmail.com>
2020-12-17 07:15:58 -0800

    final-commit

---
+++ src/Nav.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Logo_Netflix.png/1200px-Logo_Netflix.png"
+        alt="Netflix Logo"
+        className="nav__logo"
+      />
+
+      <img
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/1bdc9a33850498.56ba69ac2ba5b.png"
+        alt="avatar"
+        className="nav__avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCdrmgaXZOLXfL9koB_UJi1X11EyvLadK8",
+  authDomain: "netflix-5c187.firebaseapp.com",
+  databaseURL: "https://netflix-5c187.firebaseio.com",
+  projectId: "netflix-5c187",
+  storageBucket: "netflix-5c187.appspot.com",
+  messagingSenderId: "52506858138",
+  appId: "1:52506858138:web:c97b0336a622cc2a3cc523",
+  measurementId: "G-GN1HDC6PK0",
+};
+
+export const firebaseAnalytics = firebase.analytics();
